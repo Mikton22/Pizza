d3cd8b28a3be9486910dd3c8e9be27e77baf87ea
Mikhail Spirin <mike144513@gmail.com>
2026-04-14 12:53:23 +0500

    thunk-api

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { CartItem } from "../components/CartItem";
-import { clearItem, selectCart } from "../redux/slices/cartSlice";
+import { clearItem } from "../redux/slices/cartSlice";
 import { CartEmpty } from '../components/CartEmpty';
 
 const Cart = () => {
